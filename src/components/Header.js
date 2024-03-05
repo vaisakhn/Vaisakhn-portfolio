b5c76db69b04df7221867dac91dd885afad1c508
vaisakhn <vaisakhn02@gmail.com>
2024-03-05 12:45:45 +0530

    corrected header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ export default function Header() {
         <a className="font-bold text-black" href="#">VAISAKH N</a>
         <nav className="hidden md:block">
             <ul className="flex text-white ">
-                <li><a href="/">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#projects">Projects</a></li>
-                <li><a href="/#resume">Resume</a></li>
-                <li><a href="/#contact">Contact</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
         {toggleMenu && <nav className="block md:hidden ">
@@ -20,6 +20,7 @@ export default function Header() {
                 <li><a href="#">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>}
